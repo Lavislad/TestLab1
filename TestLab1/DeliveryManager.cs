308ab8506fbce3c3947e929b0aed0c6f2d225025
Lavislad <vladislav.mnac@gmail.com>
2026-03-05 10:57:53 +0300

    added class DeliveryManager

---
+++ TestLab1/DeliveryManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TestLab1
+{
+    internal class DeliveryManager
+    {
+        public List<Delivery> Deliveries { get; private set; }
+        public DeliveryManager()
+        {
+            Deliveries = new List<Delivery>();
+            LoadDeliveries();
+        }
+        public void AddDelivery(Delivery delivery)
+        {
+            if (delivery == null)
+            {
+                throw new ArgumentNullException(nameof(delivery));
+            }
+            Deliveries.Add(delivery);
+            SaveDeliveries();
+        }
+        public void RemoveDelivery(Delivery delivery)
+        {
+            if (delivery == null)
+            {
+                throw new ArgumentNullException(nameof(delivery));
+            }
+            Deliveries.Remove(delivery);
+            SaveDeliveries();
+        }
+        public void UpdateDeliveryStatus(Delivery delivery, DeliveryStatus newStatus)
+        {
+            if (delivery == null)
+            {
+                throw new ArgumentNullException(nameof(delivery));
+            }
+            delivery.UpdateStatus(newStatus);
+            SaveDeliveries();
+        }
+        private void SaveDeliveries()
+        {
+            File.WriteAllLines("deliveries.txt", Deliveries.Select(d =>
+            $"{d.CustomerName}|{d.Address}|{d.DeliveryDate.ToString("yyyy-MM-dd")}|{(int)d.Status}"));
+        }
+        private void LoadDeliveries()
+        {
+            if (File.Exists("deliveries.txt"))
+            {
+                var lines = File.ReadAllLines("deliveries.txt");
+                foreach (var line in lines)
+                {
+                    var parts = line.Split('|');
+                    if (parts.Length == 4)
+                    {
+                        DateTime deliveryDate;
+                        DeliveryStatus status;
+                        //if (DateTime.TryParse(parts[2], out deliveryDate) && Enum.TryParse(typeof(DeliveryStatus), parts[3], out status))
+                        //{
+                        //    Deliveries.Add(new Delivery(parts[0], parts[1], deliveryDate));
+                        //    Deliveries.Last().Status = status;
+                        //}
+                        if (DateTime.TryParse(parts[2], out deliveryDate) && Enum.TryParse(parts[3], out status)) // Просто уберите typeof(...) и лишнюю скобку
+                        {
+                            var delivery = new Delivery(parts[0], parts[1], deliveryDate);
+                            delivery.Status = status;
+                            Deliveries.Add(delivery);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
